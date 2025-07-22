6fefdf342ec1b3221cf74175914ec6e541c2145a
ussaliougueye <sn.saliougueye@gmail.com>
2025-07-22 17:12:50 -0400

    encore parfait

--- back-end/index.js
+++ back-end/index.js
@@ -84,7 +84,7 @@ app.get("/get/:email", async (req, res) => {
     const email = req.params.email;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(404).json({ error: "User not found" });
+      return res.status(404).json({ error: "User not found." });
     }
     res.json(user);
   } catch (err) {
